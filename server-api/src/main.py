f564d7bb1ad14a911dc74aa5be599aa01cb68f7c
ChadOW <chad.estoup@gmail.com>
2023-05-09 16:24:57 +0200

    OCR complete

--- server-api/src/main.py
+++ server-api/src/main.py
@@ -1,5 +1,6 @@
+from typing import Dict
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, UploadFile
 import pytesseract
 from PIL import Image
 
@@ -17,17 +18,43 @@ app.add_middleware(
 
 
 @app.post("/full", tags=["OCR", "Solving"])
-async def full():
-    return None
+async def full(image: UploadFile):
+    img = Image.open(image.file)
+    return solve_inequation(ocr_prediction(img, one_line=True, compacted=True)["text"])
 
 @app.post("/solve", tags=["Solving"])
 async def solve(inequation: str):
-    return None
+    return solve_inequation(inequation)
 
 @app.post("/ocr", tags=["OCR"])
-async def ocr(image: UploadFile):
+async def ocr(image: UploadFile, one_line: bool = True, compacted: bool = False):
     img = Image.open(image.file)
+    return ocr_prediction(img, one_line=one_line, compacted=compacted)
+
+def ocr_prediction(img: Image, one_line: bool = False, compacted: bool = False) -> Dict[str, str]:
+    text = pytesseract.image_to_string(img)
+    if one_line:
+        text = one_line_process(text)
+    if compacted:
+        text = compacted_process(text)
     return {
-            "text": pytesseract.image_to_string(img),
-            "info": pytesseract.image_to_osd(img),
-        }
+        "text": text,
+        # Info bug if dpi not perfect
+        # "info": pytesseract.image_to_osd(img),
+    }
+
+def one_line_process(text: str) -> str:
+    text = text.replace("\n", " ").replace("\f", "")
+    while "  " in text:
+        text = text.replace("  ", " ")
+    return text
+
+def compacted_process(text: str) -> str:
+    return text.replace(" ", "")
+
+def solve_inequation(inequation: str) -> Dict[str, str]:
+    return {
+        "base": inequation,
+        "result": None,
+        "steps": None,
+    }
---
+++ server-api/src/solver.py
@@ -0,0 +1,5 @@
+def solve(text: str):
+    pass
+
+def split(text: str):
+    pass
