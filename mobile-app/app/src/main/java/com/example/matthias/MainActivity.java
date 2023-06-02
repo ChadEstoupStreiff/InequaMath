46d038ce5666236825a751800f406ff36a7cae82
Elouan Lahellec <elouan.lahellec@gmail.com>
2023-06-02 16:47:21 +0200

    Ouverture de la camera depuis l'application

--- mobile-app/app/src/main/java/com/example/matthias/MainActivity.java
+++ mobile-app/app/src/main/java/com/example/matthias/MainActivity.java
@@ -2,15 +2,20 @@ package com.example.matthias;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.hardware.Camera;
 
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 
+import androidx.core.content.FileProvider;
 import androidx.core.view.WindowCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -18,43 +23,105 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.matthias.databinding.ActivityMainBinding;
+import com.example.matthias.BuildConfig;
 
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_IMAGE_CAPTURE = 1;
-    private Button btnTakePhoto;
+    private static final int REQUEST_PERMISSION_CAMERA = 2;
+
+    private Button captureButton;
+    private ImageView capturedImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnTakePhoto = findViewById(R.id.btn_take_photo);
-        btnTakePhoto.setOnClickListener(new View.OnClickListener() {
+        captureButton = findViewById(R.id.capture_button);
+        capturedImageView = findViewById(R.id.captured_image_view);
+
+        captureButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, REQUEST_PERMISSION_CAMERA);
+                } else {
+                    dispatchTakePictureIntent();
                 }
             }
         });
     }
 
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
-            // Envoyer la photo à l'API
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            capturedImageView.setImageBitmap(imageBitmap);
+
+            // Enregistrer l'image capturée dans le stockage de l'appareil avec FileProvider
+            try {
+                File imageFile = createImageFile();
+                Uri imageUri = FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID + ".fileprovider", imageFile);
+
+                OutputStream outputStream = getContentResolver().openOutputStream(imageUri);
+                imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+                outputStream.close();
+
+                // Autoriser l'accès en lecture à l'application de galerie
+                Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                mediaScanIntent.setData(imageUri);
+                sendBroadcast(mediaScanIntent);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
+    private File createImageFile() throws IOException {
+        // Créer un nom de fichier unique
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
 
+        // Obtenir le répertoire de stockage de l'application
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
 
+        // Créer le fichier
+        return File.createTempFile(imageFileName, ".jpg", storageDir);
+    }
 
 }
+
 
